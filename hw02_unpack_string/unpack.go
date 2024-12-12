ff4ac33855dfea90975f0483a8a879d5239feb2e
Evgeniy Savich <e.savich@cdek.ru>
2024-12-12 16:43:17 +0700

    hw2 without *

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,45 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	runes := []rune(s)
+
+	var result strings.Builder
+	var next rune
+
+	for i := 0; i < len(runes); i++ {
+		current := runes[i]
+		if i < len(runes)-1 {
+			next = runes[i+1]
+		} else {
+			next = 0
+		}
+
+		// cant start with digit
+		if i == 0 && unicode.IsDigit(current) {
+			return "", ErrInvalidString
+		}
+		// two digits in a row
+		if unicode.IsDigit(current) && unicode.IsDigit(next) {
+			return "", ErrInvalidString
+		}
+		// if current is digit
+		if unicode.IsDigit(next) {
+			nextAsInt, _ := strconv.Atoi(string(next))
+			result.WriteString(strings.Repeat(string(current), nextAsInt))
+			continue
+		}
+		if !unicode.IsDigit(current) {
+			result.WriteRune(current)
+		}
+	}
+
+	return result.String(), nil
 }

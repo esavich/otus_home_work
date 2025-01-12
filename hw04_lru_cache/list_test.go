cfc54416dfcd033431aab85f4b15fc9195cc6c5e
Evgeniy Savich <e.savich@cdek.ru>
2025-01-12 17:02:24 +0700

    add more list tests

--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -48,4 +48,39 @@ func TestList(t *testing.T) {
 		}
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
 	})
+
+	t.Run("one item is front and back", func(t *testing.T) {
+		l := NewList()
+		l.PushFront(10)
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, l.Front(), l.Back())
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 10, l.Back().Value)
+	})
+
+	t.Run("test remove", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10) // [10]
+		l.PushBack(20)  // [10, 20]
+		l.PushBack(30)  // [10, 20, 30]
+		require.Equal(t, 3, l.Len())
+
+		first := l.Front() // 10
+		l.Remove(first)    // [20, 30]
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, 20, l.Front().Value)
+		require.Equal(t, 30, l.Back().Value)
+
+		last := l.Back() // 30
+		l.Remove(last)   // [20]
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, 20, l.Front().Value)
+		require.Equal(t, 20, l.Back().Value)
+
+		l.Remove(l.Front()) // []
+		require.Equal(t, 0, l.Len())
+		require.Nil(t, l.Front())
+		require.Nil(t, l.Back())
+	})
 }

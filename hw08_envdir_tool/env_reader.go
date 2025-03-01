013f7c84c8ed1d968d3fdfaf563b1d1c7524ec02
Evgeniy Savich <e.savich@cdek.ru>
2025-03-01 18:09:57 +0700

    hw08 implement env reader

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -12,5 +18,40 @@ type EnvValue struct {
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
 	// Place your code here
-	return nil, nil
+	dirContent, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, fmt.Errorf("error reading directory: %w", err)
+	}
+
+	env := make(Environment, len(dirContent))
+
+	for _, fileInfo := range dirContent {
+		if fileInfo.IsDir() {
+			continue
+		}
+		fileName := fileInfo.Name()
+
+		if strings.Contains(fileName, "=") {
+			continue
+		}
+
+		fileContent, err := os.ReadFile(dir + "/" + fileName)
+		if err != nil {
+			return nil, fmt.Errorf("error opening file: %w", err)
+		}
+		firstLine, _, _ := strings.Cut(string(fileContent), "\n")
+		firstLine = strings.TrimRight(firstLine, "\t ")
+		firstLine = strings.ReplaceAll(firstLine, "\x00", "\n")
+
+		info, err := fileInfo.Info()
+		if err != nil {
+			return nil, fmt.Errorf("error getting file info: %w", err)
+		}
+		env[fileName] = EnvValue{
+			Value:      firstLine,
+			NeedRemove: info.Size() == 0,
+		}
+	}
+
+	return env, nil
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 	// Place your code here.
+	args := os.Args
+	fmt.Println(args)
+
+	if len(args) < 3 {
+		fmt.Println("to few arguments")
+		fmt.Println("Usage: go-envdir path/to/env/dir command arg1 arg2 ...")
+		os.Exit(1)
+	}
+
+	envDir := args[1]
+	cmd := args[2:]
+
+	fmt.Printf("envdir: %s, cmd and args %s\n", envDir, cmd)
+
+	env, err := ReadDir(envDir)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(env)
 }
